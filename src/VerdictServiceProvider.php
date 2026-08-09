f8ad7ee5e5692a35ac1623e3c7a2fcfba2e58b0a
Allen McCabe <allenmccabe@gmail.com>
2026-08-09 11:34:34 -0700

    feat: publish invocation evidence migration

--- src/VerdictServiceProvider.php
+++ src/VerdictServiceProvider.php
@@ -251,6 +251,7 @@ final class VerdictServiceProvider extends ServiceProvider
         $evidenceMigration = [
             __DIR__.'/../database/migrations/create_verdict_evidence_table.php.stub' => database_path('migrations/2026_08_01_000001_create_verdict_evidence_table.php'),
             __DIR__.'/../database/migrations/add_provenance_to_verdict_evidence_table.php.stub' => database_path('migrations/2026_08_01_000004_add_provenance_to_verdict_evidence_table.php'),
+            __DIR__.'/../database/migrations/add_invocation_id_to_verdict_evidence_table.php.stub' => database_path('migrations/2026_08_09_000005_add_invocation_id_to_verdict_evidence_table.php'),
         ];
         $rateLimitMigration = [
             __DIR__.'/../database/migrations/create_verdict_rate_limit_buckets_table.php.stub' => database_path('migrations/2026_08_01_000002_create_verdict_rate_limit_buckets_table.php'),
